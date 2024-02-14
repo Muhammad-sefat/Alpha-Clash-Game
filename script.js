c7738ef3b65399feb4c5c0d782902c436c0c1163
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-02-14 16:55:05 +0600

    Display random Alphabet

--- script.js
+++ script.js
@@ -3,4 +3,26 @@ function startPlay(){
     const secondDiv = document.getElementById('second-div');
     firstDiv.classList.add('hidden');
     secondDiv.classList.remove('hidden');
+
+
+    const alphabet = getRandomAlphabets();
+    const showAlphabet = document.getElementById('show-alphabet');
+    showAlphabet.innerText = alphabet;
+
+    const randomColor = setRandomColor(alphabet);
+    randomColor();
+}
+
+function setRandomColor(elementId){
+    let element = document.getElementById(elementId)
+    element.classList.add('bg-orange-400');
+}
+
+function getRandomAlphabets(){
+    const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+    let alphabets = alphabetString.split('');
+    let number = Math.round(Math.random() * 25);
+    let alphabet = alphabets[number];
+    console.log(alphabet);
+    return alphabet;
 }
