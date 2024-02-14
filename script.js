fc6497417fbeeff860ade65086b5dba5800421c6
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-02-14 16:19:27 +0600

    Add Toggle

---
+++ script.js
@@ -0,0 +1,6 @@
+function startPlay(){
+    const firstDiv = document.getElementById('first-div');
+    const secondDiv = document.getElementById('second-div');
+    firstDiv.classList.add('hidden');
+    secondDiv.classList.remove('hidden');
+}
